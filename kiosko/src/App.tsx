0eebc349158d58497338bfd8e6983be65c3bf4a4
gabriel <gabrielmedinatorres@hotmail.com>
2025-02-04 10:16:18 +0000

    cambios en css

--- kiosko/src/App.tsx
+++ kiosko/src/App.tsx
@@ -3,13 +3,23 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <div className="QR"></div>
-      <div className="Consumo Agua y Luz"></div>
-      <div className="Logos"></div>
-      <div className="LUZ"></div>
-      <div className="Agua"></div>
-      <div className="Consumo"></div>
-      <div className="ConsumoAgua"></div>
+      <div className="cabecera">
+        <div className="QR">QR</div>
+        <div className="Consumo_Agua_y_Luz">
+          <h1>Consumo de agua y luz</h1>
+        </div>
+        <div className="Logos">LOGO</div>
+      </div>
+
+      <div className="carteles">
+        <div className="LUZ"></div>
+        <div className="Agua"></div>
+      </div>
+
+      <div className="consumogeneral">
+        <div className="Consumo"></div>
+        <div className="ConsumoAgua"></div>
+      </div>
     </div>
   );
 }
