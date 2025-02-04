18ae365675362941b7c29f8de8ff5435e4da3207
gabriel <gabrielmedinatorres@hotmail.com>
2025-02-04 08:57:12 +0000

    Incio el proyecto

---
+++ kiosko/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="QR"></div>
+      <div className="Consumo Agua y Luz"></div>
+      <div className="Logos"></div>
+      <div className="LUZ"></div>
+      <div className="Agua"></div>
+      <div className="Consumo"></div>
+      <div className="ConsumoAgua"></div>
+    </div>
+  );
+}
+
+export default App;
